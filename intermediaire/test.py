f3a603b3d95e510d396a93b36ab98d1ac9a1c525
LEPRINCE Enzo <enzo.leprince@etu.univ-littoral.fr>
2023-10-24 15:12:47 +0200

    Petit changement

--- intermediaire/test.py
+++ intermediaire/test.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Charger l'image astronomique
-image_astronomique = cv2.imread('./fichiers ressource-20231023/barnard_stacked_gradient.png')
+image_astronomique = cv2.imread('fichiers ressource-20231023/lumiere.jpg')
 
 # Convertir l'image en niveaux de gris pour faciliter le traitement
 gray_image = cv2.cvtColor(image_astronomique, cv2.COLOR_BGR2GRAY)
@@ -14,7 +14,7 @@ _, pollution_mask = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
 non_pollution_mask = cv2.bitwise_not(pollution_mask)
 
 # Assombrir la région sans pollution lumineuse
-image_astronomique[non_pollution_mask > 0] = image_astronomique[non_pollution_mask > 0] * 0.3
+image_astronomique[non_pollution_mask > 0] = image_astronomique[non_pollution_mask > 0] * 0.2
 
 # Enregistrer l'image résultante
 cv2.imwrite('image_mis_en_evidence.jpg', image_astronomique)
