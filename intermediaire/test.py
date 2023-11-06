f8c7c630e53eeb4626ed56704bc28d1b991e3a31
LEPRINCE Enzo <enzo.leprince@etu.univ-littoral.fr>
2023-11-06 09:20:14 +0100

    Modification test

--- intermediaire/test.py
+++ intermediaire/test.py
@@ -1,20 +1,47 @@
 import cv2
 import numpy as np
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 
-# Charger l'image astronomique
-image_astronomique = cv2.imread('fichiers ressource-20231023/lumiere.jpg')
+def main():
+    # Chargement d'une image astronomique
+    image_astronomique = cv2.imread('fichiers ressource-20231023/barnard_stacked_gradient.png')
 
-# Convertir l'image en niveaux de gris pour faciliter le traitement
-gray_image = cv2.cvtColor(image_astronomique, cv2.COLOR_BGR2GRAY)
+    # Convertion d'une image en niveaux de gris pour faciliter le traitement
+    gray_image = cv2.cvtColor(image_astronomique, cv2.COLOR_BGR2GRAY)
 
-# Appliquer un seuil pour détecter la pollution lumineuse (vous devrez ajuster ce seuil)
-_, pollution_mask = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
+    # Appliquage d'un seuil pour détecter la pollution lumineuse (vous devrez ajuster ce seuil)
+    _, pollution_mask = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)
 
-# Inverser le masque pour détecter la région sans pollution lumineuse
-non_pollution_mask = cv2.bitwise_not(pollution_mask)
+    # Inversion du masque pour détecter la région sans pollution lumineuse
+    non_pollution_mask = cv2.bitwise_not(pollution_mask)
 
-# Assombrir la région sans pollution lumineuse
-image_astronomique[non_pollution_mask > 0] = image_astronomique[non_pollution_mask > 0] * 0.2
+    # Assombrissement de la région sans pollution lumineuse
+    image_astronomique[non_pollution_mask > 0] = image_astronomique[non_pollution_mask > 0] * 0.1
 
-# Enregistrer l'image résultante
-cv2.imwrite('image_mis_en_evidence.jpg', image_astronomique)
+    # Enregistrement de l'image résultante
+    cv2.imwrite('image_mis_en_evidence.jpg', image_astronomique)
+
+    # Création d'une fenêtre Tkinter
+    fenetre = tk.Tk()
+    fenetre.title("Image Astronomique Modifiée")
+    fenetre.attributes('-fullscreen', True) 
+    
+    # Redimensionnenement de l'image pour l'affichage
+    image_astronomique = cv2.cvtColor(image_astronomique, cv2.COLOR_BGR2RGB)
+    image_astronomique = Image.fromarray(image_astronomique)
+    image_astronomique = image_astronomique.resize((1000, 700))
+    photo = ImageTk.PhotoImage(image=image_astronomique)
+
+    # Création d'un label pour afficher l'image
+    label = ttk.Label(fenetre, image=photo)
+    label.pack()
+
+    fenetre.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+# qu'est ce qu'on a appris
